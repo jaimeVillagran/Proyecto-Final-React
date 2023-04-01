9ef7acf6eb750e2dc78c4835dc9a569beb192c55
TJaime <jaime.villagran@gmail.com>
2023-04-01 19:46:17 -0300

    Configuracion Archivos

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	server: {
+		port: 3030,
+	},
+	preview: {
+		port: 4270,
+	},
+	build: {
+		incremental: true,
+	},
+	babel: {
+		presets: ["@babel/presets-env", "@babel / presets - react"],
+	},
+});
